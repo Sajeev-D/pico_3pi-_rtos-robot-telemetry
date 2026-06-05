a68b90854a77b99478a948ca1603d386119a7f04
Sajeev Debnath <sajeevbd04@gmail.com>
2026-06-05 17:44:35 -0400

    used API functions to profile dummy function and added a README file

---
+++ serial_IO/README.md
@@ -0,0 +1,3 @@
+# Testing serial IO and simple profiling of a function using absolute time functions.
+
+Documentation of API details: https://www.raspberrypi.com/documentation/pico-sdk/high_level.html#group_pico_time
--- serial_IO/serial_IO.c
+++ serial_IO/serial_IO.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+int counter = 0;
+
+int dummy(void) {
+    counter++;  
+    for (int i = 0; i < 1000000; i++) {
+        // Just a busy loop to consume some time
+        counter += 1;
+    }
+    
+    int a = 8;
+    int b = 10;
+    return a + b;
+}
 
 int main()
 {
     stdio_init_all();
 
     while (true) {
-        
-        printf("Hello, world!\n");
+
+        absolute_time_t t = get_absolute_time();
+        dummy();
+        absolute_time_t t2 = get_absolute_time();
+        int64_t elapsed = absolute_time_diff_us(t, t2);
+        printf("Elapsed time: %lld microseconds\n", elapsed);
+
+
+        // printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
